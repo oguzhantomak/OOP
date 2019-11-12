a36d25e74243e0eb8a19a7657e73fc3a3dfb1dd8
oguzhantomak <oguzhantomak@gmail.com>
2019-11-12 13:41:57 +0300

    Add project files.

---
+++ OOP_Class/FirstClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class
+{
+    class FirstClass
+    {
+        public int Id;
+        public string Ad;
+        public string SoyAd;
+
+        public int Kilo;
+        public int Boy;
+
+        //public int Yas;
+        private int _yas;
+        public int Yas {
+            get
+            {
+                return _yas;
+            }
+            set
+            {
+                if (value<18 && value>55)
+                {
+                    _yas = value;
+                }
+                else
+                {
+                    _yas = 0;
+                }
+            }
+        }
+
+        //public void Set(int yas)
+        //{
+        //    if (yas < 18 && yas > 55)
+        //    {
+        //        _yas = 0;
+        //    }
+        //    else
+        //    {
+        //        _yas = yas;
+        //    }
+        //}
+        //public int Get()
+        //{
+        //    return _yas;
+        //}
+
+    }
+}
---
+++ OOP_Class/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Instance almak, yeni bir örnek alma.
+            FirstClass fc1 = new FirstClass();
+            fc1.Ad = "Oğuzhan";
+            fc1.SoyAd = "Tomak";
+            fc1.Yas = 20;
+
+            label1.Text = fc1.Ad;
+            label2.Text = fc1.SoyAd;
+        }
+    }
+}
